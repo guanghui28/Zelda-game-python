dbc31675f76bcd85d4160ef3105225aaf592af5f
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-09-07 11:38:55 +0700

    add weapon

--- code/player.py
+++ code/player.py
@@ -3,7 +3,7 @@ from settings import *
 from support import import_folder
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self,pos,groups, obstacle_sprites):
+	def __init__(self,pos,groups, obstacle_sprites, create_attack, destroy_attack):
 		super().__init__(groups)
 		self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
@@ -22,6 +22,15 @@ class Player(pygame.sprite.Sprite):
 		self.attack_time = None
   
 		self.obstacle_sprites = obstacle_sprites
+  
+		# weapon
+		self.create_attack = create_attack
+		self.destroy_attack = destroy_attack
+		self.weapon_index = 0
+		self.weapon = list(weapon_data.keys())[self.weapon_index]
+		self.can_switch_weapon = True
+		self.weapon_switch_time = None
+		self.switch_duration_cooldown = 200
   
 	def import_player_assets(self):
 		character_path = '../graphics/player'
@@ -63,11 +72,19 @@ class Player(pygame.sprite.Sprite):
 			if keys[pygame.K_SPACE]:
 				self.attacking = True
 				self.attack_time = pygame.time.get_ticks()
+				self.create_attack()
 	
 			# magic input
 			if keys[pygame.K_LCTRL]:
 				self.attacking = True
 				self.attack_time = pygame.time.get_ticks()
+    
+			# choose weapon
+			if keys[pygame.K_q] and self.can_switch_weapon:
+				self.can_switch_weapon = False
+				self.weapon_switch_time = pygame.time.get_ticks()
+				self.weapon_index = (self.weapon_index + 1) % len(weapon_data.keys())
+				self.weapon = list(weapon_data.keys())[self.weapon_index]
 
 	def movement(self, speed):
 		if self.direction.magnitude() != 0:
@@ -117,9 +134,15 @@ class Player(pygame.sprite.Sprite):
  
 	def cooldown(self):
 		current_time = pygame.time.get_ticks()
+  
 		if self.attacking:
 			if current_time - self.attack_time > self.attack_cooldown:
 				self.attacking = False
+				self.destroy_attack()
+    
+		if not self.can_switch_weapon:
+			if current_time - self.weapon_switch_time > self.switch_duration_cooldown:
+				self.can_switch_weapon = True
  
 	def animate(self):
 		animations_surface = self.animations[self.status]
