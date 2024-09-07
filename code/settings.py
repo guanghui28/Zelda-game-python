0710459a94c23a576f9c41feb4f12ed9e4f50dc2
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-09-07 13:44:03 +0700

    ui

--- code/settings.py
+++ code/settings.py
@@ -13,6 +13,25 @@ weapon_data = {
     'sai':{'cooldown': 80, 'damage':10, 'graphic': '../graphics/weapons/sai/full.png'},
 }
 
+# ui
+BAR_HEIGHT = 20
+HEALTH_BAR_WIDTH = 200
+ENERGY_BAR_WIDTH = 140
+ITEM_BOX_SIZE = 80
+UI_FONT = '../graphics/font/joystix.ttf'
+UI_FONT_SIZE = 18
+
+# general colors
+WATER_COLOR = '#71DDEE'
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = '#111111'
+TEXT_COLOR = '#EEEEEE'
+
+# ui colors
+HEALTH_COLOR = 'red'
+ENERGY_COLOR = 'blue'
+UI_BORDER_COLOR_ACTIVE = 'gold'
+
 # WORLD_MAP = [
 # ['x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x'],
 # ['x',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
